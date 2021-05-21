f9bd405e62a2fb2fd98eaeba03140e2a60270ac3
Schmek704 <dmckeonvol@gmail.com>
2021-05-21 20:53:58 +0000

    update thorugh module3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,8 +5,8 @@ from .models import CarModel, CarMake
 # Register your models here.
 
 # CarModelInline class
-# class CarModelInline(admin.StackedInline):
-#     model = CarMake
+class CarModelInline(admin.StackedInline):
+    model = CarMake
 
 
 # CarModelAdmin class
