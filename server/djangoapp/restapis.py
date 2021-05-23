91faeafdccabbab216eb7fcc1c5b2d4ec31f24d2
Dan McKeon <78289818+Schmek704@users.noreply.github.com>
2021-05-22 20:52:14 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -86,7 +86,7 @@ def get_dealers_from_cf(url, **kwargs):
     json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
-        dealers = json_result["dealerships"]
+        dealers = json_result[]
         # For each dealer object
         for dealer in dealers:
             # Get its content in `doc` object
